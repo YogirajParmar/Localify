b43b847876d2ac7f34c3469132d4b7a91daecb5c
YogirajParmar <138298164+YogirajParmar@users.noreply.github.com>
2023-07-28 17:25:05 +0530

    Add files via upload

---
+++ Assets/php/about.php
@@ -0,0 +1,77 @@
+<?php
+if(!isset($_COOKIE["user"])){
+    header("location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.rtl.min.css"
+      integrity="sha384-T5m5WERuXcjgzF8DAb7tRkByEZQGcpraRTinjpywg37AO96WoYN9+hrhDVoM6CaT"
+      crossorigin="anonymous"
+    />
+    <link href="assets/css/userdashboard.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/carousel.css" rel="stylesheet" type="text/css" />
+<style>
+    .aboutus , .who_are_we{
+        
+    }
+    .container-fluid{
+    display: flex;
+    flex-wrap: inherit;
+    align-items: center;
+    justify-content: space-between;
+    }
+    </style>
+
+  </head>
+  <link rel="stylesheet" href="https://classless.de/classless.css">
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+    crossorigin="anonymous"
+  />
+
+  
+
+
+<body>
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+   <div class="container-fluid">
+    <img src="image1.png" style="height:50px;margin-right:2px;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav" >
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="create.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>  
+    <br>  
+ </div>
+<div>
+    <h2>aa</h2>
+</div>
+</body>
